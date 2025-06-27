0ea019c560d9fbe82a82e99dc9c76a11f847ca69
B. Dhinesh kumar <77185255+dhinesh-b@users.noreply.github.com>
2025-06-26 23:05:35 -0700

    Add files via upload

---
+++ includes/admin_header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
---
+++ includes/sidebar.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+    <h2>ADMISSIONS</h2>
+    <a href="admin_dashboard.php">Dashboard</a>
+    <a href="applications.php">Applications</a>
+    <a href="courses.php">Course Management</a>
+    <a href="logout.php">Logout</a>
+</div>
